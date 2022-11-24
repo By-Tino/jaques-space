1000244c0a62bd8a6c29ced914f8ba7fa86d4685
jaques tino <daily-record@qq.com>
2022-11-24 19:52:27 +0800

    更新 tino-navigator 组件

--- src/components/TinoNavigator/index.vue
+++ src/components/TinoNavigator/index.vue
@@ -2,9 +2,9 @@
   <view :class="[ namespace ]">
     <view :class="[ namespace + '__wrapper' ]">
       <slot name="suffix-icon">
-        <tino-icon name="left-light" :size="50" color="#fff" @click="goBack" />
+        <tino-icon name="left-light" :size="50" :color="color" @click="goBack" />
       </slot>
-      <view :class="[ namespace + '__wrapper_content' ]">
+      <view :class="[ namespace + '__wrapper_content' ]" :style="{ color }">
         <slot />
       </view>
     </view>
@@ -16,7 +16,14 @@
 
 import { useNamespace } from '@/hooks'
 
+interface NavigatorProps {
+  color?: string
+}
+
 const namespace = useNamespace('navigator')
+const props = withDefaults(defineProps<NavigatorProps>(), {
+  color: '#333'
+})
 
 const goBack = () => {
   uni.navigateBack()
